240bb6ecc25ca9caffdb58c4a3f619b475201640
Ricardo Padilha <ricardospadilha@gmail.com>
2015-07-24 07:15:45 -0700

    Updated build scripts, added -lrt to lxml build

--- app.sh
+++ app.sh
@@ -46,8 +46,8 @@ export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
 PKG_CONFIG_PATH="${XPYTHON}/lib/pkgconfig" \
-  LDSHARED="${CC} -shared -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${XPYTHON}/lib" \
-  "${XPYTHON}/bin/python" setup.py build_ext \
+  LDFLAGS="${LDFLAGS:-} -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${XPYTHON}/lib" \
+  "${XPYTHON}/bin/python" setup.py build_ext -lrt \
   --include-dirs="${XPYTHON}/include" --library-dirs="${XPYTHON}/lib" \
   --force build --force bdist_egg --dist-dir "${BASE}" \
   --with-xslt-config="${DEPS}/bin/xslt-config"
